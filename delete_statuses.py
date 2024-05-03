832e275dca964f7717ab2d2849419a97d0cafd14
Ricardo Amendoeira <ramendoeira@salsify.com>
2024-05-03 11:46:32 +0100

    Add access token

--- delete_statuses.py
+++ delete_statuses.py
@@ -1,14 +1,18 @@
 import os
+import sys
 from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 from mastodon import Mastodon
+
+since_hours_ago = int(sys.argv[1] if len(sys.argv) > 1 else 1)
+print(f'Since {since_hours_ago} hours ago')
 
 load_dotenv()
 MASTODON_ACCESS_TOKEN = os.environ['MASTODON_ACCESS_TOKEN']
 
 m = Mastodon(access_token=MASTODON_ACCESS_TOKEN, api_base_url="https://masto.pt")
 
-statuses_to_delete = [s for s in m.timeline() if s['created_at'] >= datetime.now(timezone.utc) - timedelta(hours=1)]
+statuses_to_delete = [s for s in m.timeline() if s['created_at'] >= datetime.now(timezone.utc) - timedelta(hours=since_hours_ago)]
 
 # input(f'Delete {len(statuses_to_delete)} statuses? ^C to cancel')
 
