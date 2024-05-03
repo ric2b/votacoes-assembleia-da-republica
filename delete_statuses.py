96aa0b78d56c444fb89775990c67173f796b0666
Ricardo Amendoeira <ramendoeira@salsify.com>
2024-05-03 11:53:31 +0100

    Use minutes instead of hours

--- delete_statuses.py
+++ delete_statuses.py
@@ -4,17 +4,17 @@ from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 from mastodon import Mastodon
 
-since_hours_ago = int(sys.argv[1] if len(sys.argv) > 1 else 1)
-print(f'Since {since_hours_ago} hours ago')
+since_minutes_ago = int(sys.argv[1] if len(sys.argv) > 1 else 1)
 
 load_dotenv()
 MASTODON_ACCESS_TOKEN = os.environ['MASTODON_ACCESS_TOKEN']
 
 m = Mastodon(access_token=MASTODON_ACCESS_TOKEN, api_base_url="https://masto.pt")
 
-statuses_to_delete = [s for s in m.timeline() if s['created_at'] >= datetime.now(timezone.utc) - timedelta(hours=since_hours_ago)]
+statuses_to_delete = [s for s in m.timeline() if s['created_at'] >= datetime.now(timezone.utc) - timedelta(minutes=since_minutes_ago)]
 
 # input(f'Delete {len(statuses_to_delete)} statuses? ^C to cancel')
+print(f'Deleting {len(statuses_to_delete)} statuses since {since_minutes_ago} minutes ago')
 
 for status in statuses_to_delete:
     id = status['id']
