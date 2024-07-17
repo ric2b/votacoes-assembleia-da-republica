6db2aee48826a5d0496aed2c07cc3d09f2c45469
Ricardo Amendoeira <ramendoeira@salsify.com>
2024-07-17 12:25:38 +0100

    Create a thread for each result group (#2)

--- votacoes_assembleia_da_republica/update_account.py
+++ votacoes_assembleia_da_republica/update_account.py
@@ -10,6 +10,19 @@ from votacoes_assembleia_da_republica.fetch_votes import fetch_votes_for_legisla
 MARK_ALL_AS_PUBLISHED = os.environ.get('MARK_ALL_AS_PUBLISHED', 'false').lower() == 'true'
 OVERRIDE_TOO_MANY_NEW_VOTES_CHECK = os.environ.get('OVERRIDE_TOO_MANY_NEW_VOTES_CHECK', 'false').lower() == 'true'
 
+def render_thread(result: str, sorted_new_votes: list[dict]) -> str:
+    date_start = sorted_new_votes[0]['date']
+    date_end = sorted_new_votes[-1]['date']
+
+    with open('vote_thread.template', 'r') as thread_template_file:
+        thread_template = Template(thread_template_file.read())
+
+        return thread_template.substitute(
+            result = '🟢 Aprovadas' if result == 'Aprovado' else '🔴 Rejeitadas',
+            date_start = date_start,
+            date_end = date_end,
+        )
+
 def render_vote(vote: dict) -> str:
     with open('vote_status.template', 'r') as toot_template_file:
         toot_template = Template(toot_template_file.read())
@@ -37,6 +50,16 @@ def render_vote(vote: dict) -> str:
             initiative_uri = vote['initiative_uri'],
         )
 
+def group_votes_by_result(votes: list[dict]) -> dict[str, list[dict]]:
+    votes_by_result = {}
+
+    for vote in votes:
+        result = vote['result']
+        votes_by_result[result] = votes_by_result.get(result, [])
+        votes_by_result[result].append(vote)
+
+    return votes_by_result
+
 def update(legislature: str, state_file_path = 'state.json'):
     with StateStorage(file_path = state_file_path) as state:
             print('fetching votes')
@@ -59,12 +82,23 @@ def update(legislature: str, state_file_path = 'state.json'):
 
             print('posting votes')
             m = MastodonClient()
-            for new_vote in sorted(new_votes, key = itemgetter('date')):
-                vote_id = new_vote['vote_id']
 
-                print(f'posting vote {vote_id}')
-                m.post_vote(render_vote(new_vote), idempotency_key = vote_id)
-                state.mark_vote_published(vote_id)
+            new_votes_by_result = group_votes_by_result(new_votes)
+
+            for result in sorted(new_votes_by_result):
+                new_votes_for_result = new_votes_by_result[result]
+                print(f'starting a thread for result {result}')
+                new_votes_for_result.sort(key = itemgetter('date'))
+
+                idempotency_key = str(hash(vote for vote in new_votes))
+                result_thread = m.start_vote_thread(render_thread(result, new_votes_for_result), idempotency_key = idempotency_key)
+
+                for new_vote_for_result in new_votes_for_result:
+                    vote_id = new_vote_for_result['vote_id']
+
+                    print(f'posting vote {new_vote_for_result}')
+                    m.post_vote(render_vote(new_vote_for_result), reply_to = result_thread, idempotency_key = vote_id)
+                    state.mark_vote_published(vote_id)
 
 if __name__ == '__main__':
     load_dotenv()
